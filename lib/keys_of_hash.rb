a3060bd2a9228503e18bbfda85f446b9b06bc197
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-28 20:16:39 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,16 +1,5 @@
 require "pry"
 
-def animals
-  animals = 
-   {"sugar glider"=>"Australia",
-   "aye-aye"=> "Madagascar",
-   "red-footed tortoise"=>"Panama",
-   "kangaroo"=> "Australia",
-   "tomato frog"=>"Madagascar",
-   "koala"=>"Australia"} 
-end 
-
-
 class Hash
   def keys_of(*arguments)
     array = []
